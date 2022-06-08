a477fe18ba52654f16d791efdb0955f5996b4d46
bakkerjoeri <bakkerjoeri@gmail.com>
2022-06-08 15:54:51 +0200

    creates breaking test case

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SaasProvider } from '@saas-ui/react'
+import { AppShell, theme } from '@saas-ui/pro'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<SaasProvider theme={theme}>
+		<AppShell>
+	<Component {...pageProps} />
+	</AppShell>
+	</SaasProvider>
+	)
 }
 
 export default MyApp
